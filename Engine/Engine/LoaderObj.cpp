55c880333751d3acac322b2b25d75b69c6c582d3
Lecomte Emerick <emerick.lecomte@gmail.com>
2015-12-24 16:06:30 +0100

    fix tiny_obj_loader

--- Engine/Engine/LoaderObj.cpp
+++ Engine/Engine/LoaderObj.cpp
@@ -1,7 +1,6 @@
 #include "LoaderObj.h"
 
-#define TINYOBJLOADER_IMPLEMENTATION
-#include "tiny_obj_loader.h"
+#include "tinyobjLoader\tiny_obj_loader.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
